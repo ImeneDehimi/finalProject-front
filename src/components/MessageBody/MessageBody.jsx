dfa8d8b373f8e94a0bb7335f386345b69b1b929e
ImeneDehimi <imenedehimi8@gmail.com>
2024-05-25 13:58:43 +0100

    ..

--- src/components/MessageBody/MessageBody.jsx
+++ src/components/MessageBody/MessageBody.jsx
@@ -5,7 +5,6 @@ import axios from 'axios';
 
 const MessageBody = ({ chat, currentUser, setSendMessage,  receivedMessage }) => {
   const [token, setToken] = useState(localStorage.getItem('token') || null);
-  console.log(token);
 
   const [userData, setUserData] = useState(null);
   const [messages, setMessages] = useState([]);
