3e2299c01ec8472e3c71c2d05012edf40b81d64f
ImeneDehimi <imenedehimi8@gmail.com>
2024-05-27 14:38:19 +0100

    ..

--- src/pages/Messages/Messages.jsx
+++ src/pages/Messages/Messages.jsx
@@ -51,7 +51,7 @@ const Messages = () => {
 
   // Connect to Socket.io
   useEffect(() => {
-    socket.current = io("https://final-project-back-i01u.onrender.com:5000");
+    socket.current = io("https://final-project-back-i01u.onrender.com");
     socket.current.emit("new-user-add", user._id);
     socket.current.on("get-users", (users) => {
       setOnlineUsers(users);
